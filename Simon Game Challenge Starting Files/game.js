ac3a829573ff55842db74f9a12cd3fe9094c8203
Jooyoung Kim <yongin016@gmail.com>
2024-09-21 13:02:16 -0600

    completed Step5, Step6

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,6 +2,18 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+// When user click any button which has btn class, it execute the function.
+// The Clicked button assign to userChoseColor and add it to userClickedPattern.
+$(".btn").click(function() {
+    
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+});
+
 
 function nextSequence() {
 
@@ -19,31 +31,24 @@ function nextSequence() {
     // My version: give an effect using jquery and id
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
+    playSound(randomChosenColour);
+
+    }
+
+
+function playSound(name) {
     // My version: Play each audio file according to randomCHosenColour
     // ChatGPT: Create a new audio object for the sound file corresponding to the randomChosenColour and play it
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+
 }
 
-// When user click any button which has btn class, it execute the function.
-// The Clicked button assign to userChoseColor and add it to userClickedPattern.
-$(".btn").on("click", function() {
-    var userChosenColor = this.id;
-    userClickedPattern.push(userChosenColor);
+function animatePress(currentColour) {
     
-    alert(userClickedPattern);
-});
-
-// wrong button sound
-    // var wrong = new Audio("./sounds/wrong.mp3");
-    // wrong.play();
-
-// Press A key to start the game
-// document.addEventListener("keydown", function(event) {
-//     if (event.key === "A" || event.key === "a") {
-//         nextSequence();
-//     } 
-// });
-
-// After pressing A key, this function will execute
-// flash effect 
+    $("#" + currentColour).addClass("pressed");
+    
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
